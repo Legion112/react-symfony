c36c2154377feef12ecdcf8ac49137ac01cedbd4
Max <ageev.maks2014@gmail.com>
2019-02-25 20:08:59 +0500

    02-react-create-element

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,14 @@
-console.log('Oh hallow React peepr!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Lift history!',
+    React.createElement('span', null, '❤')
+);
+
+console.log(el);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
+
